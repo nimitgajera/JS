d39b6bb66d2e45c3ce0256799bdc661d14d3b96d
Nimit <nimitgajera3@gmail.com>
2022-06-01 16:47:46 +0530

    1-6-22 commit

---
+++ arithmatic.js
@@ -0,0 +1,88 @@
+// let a=10
+// let b=11
+// console.log(a+b)        
+// console.log("a"+"b")   
+// console.log(a*"b")          
+// console.log("a"+b)          
+// console.log(a*'b')           
+// console.log("a"*"b")       
+// console.log("b+a")          
+// console.log(a+"b")
+// console.log(a*"b")
+wedw
+
+
+
+// console.log(true+true+false+true<1)
+// console.log(trsssssue+true+"false")
+// console.log(true+true>0)    
+// console.log(true+true<false+true<1)    
+// console.log(true+true<1)        
+// console.log(true+false+0<1)     
+// console.log(true<1)    
+
+// var a=30;
+// {
+//     var a=10;
+//     console.log(a)
+//     {
+//          console.log(a)
+//          {
+//              var a=10
+//              console.log(a)
+
+//          }    console.log(a)
+//     }
+//     {
+//         var a=20
+//         {
+//             console.log(a)
+
+//         }    console.log(a)
+
+//     }
+// }    console.log(a)
+
+
+
+// let a=30;
+// {
+//     let a=20;
+//     console.log(a)
+//     {
+//          console.log(a)
+//          {
+//             let a=10
+//              console.log(a)
+
+//          }    console.log(a)
+//     }
+//     {
+//         let a=40
+//         {
+//             console.log(a)
+
+//         }    console.log(a)
+
+//     }
+// }    console.log(a)
+
+
+// var a=10
+// var a=20
+// console.log(a)
+
+
+// var a=10
+// const b=20
+// console.log(a+b)
+
+
+// var b=11;
+// const c=22
+// console.log(b*c)
+// {
+//     var b=33
+//     const c=22
+//     console.log(b*c)
+// }    console.log(b*c)
